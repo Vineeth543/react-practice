45acf9e1cc528ed1053c531df2872c2449c284dd
vinee <vineethserigar17@gmail.com>
2021-07-13 17:04:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-6xl">Vineeth</h1>
+      <h1 className="text-8xl hover:text-blue-500">Full Stack Web Developer</h1>
+      <button className="border-2 px-2 py-1 border-blue-500 rounded bg-blue-500 text-white hover:bg-transparent hover:border-2 hover:text-blue-500 outline-none focus:outline-none focus:ring focus:border-blue-300">
+        Hello Tailwind
+      </button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";        //const React = reqire("react");
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
